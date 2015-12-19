1e7c63daa065903a2f7e34d1bb6453876d59d6e3
Kyle Walker <kew331@gmail.com>
2015-12-18 23:02:49 -0600

    fix the moe_sum function so that it works for data frames

--- R/moe.R
+++ R/moe.R
@@ -1,15 +1,37 @@
 #' Calculate the margin of error for a derived sum
 #'
+#' @param vec A vector of numbers that represent margins of error, or column names from a data frame that represent margins of error for estimates in that data frame.
+#' @param df The data frame on which to perform the calculation, if applicable.
 #' @export
-moe_sum <- function(...) {
+moe_sum <- function(vec, df = NULL) {
 
-  values <- unlist(list(...))
+  if (is.numeric(vec)) { # a single vector of numbers
 
-  s <- sapply(values, function(x) x^2)
+    s <- sapply(vec, function(x) x^2)
 
-  result <- sqrt(sum(s, na.rm = TRUE))
+    result <- sqrt(sum(s, na.rm = TRUE))
 
-  return(result)
+    return(result)
+
+  } else if (is.character(vec)) { # assuming here data frame columns
+
+    internal_fun <- function(values) {
+
+      s <- sapply(values, function(x) x^2)
+
+      result <- sqrt(sum(s, na.rm = TRUE))
+
+      return(result)
+
+    }
+
+    sub <- df[,vec]
+
+    result <- apply(sub, 1, function(x) internal_fun(x))
+
+    return(as.vector(result))
+
+  }
 
 }
 
